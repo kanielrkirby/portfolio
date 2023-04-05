582014414988ffea9cf7ca9e70974d71e7b4665a
Max <kanielrkirby@gmail.com>
2023-04-05 13:54:36 -0500

    Updated projects descriptions

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,8 +3,8 @@ export default function Projects({ className }: { className: string }) {
     {
       title: "Polychrome",
       description: {
-        short: "A simple color palette generator.",
-        long: "A simple color palette generator.",
+        short: "A versatile color palette generator with multiple algorithms.",
+        long: "Polychrome is a versatile color palette generator that features multiple color algorithms, drag and drop swatches, copy/paste functionality, and more. As my first project, it took 3 months to complete and was built using Plain TypeScript and PostCSS, with minimal reliance on external libraries.",
       },
       link: "https://piratey7007.github.io/polychrome/",
       image: "https://piratey7007.github.io/polychrome/polychrome.png",
@@ -12,12 +12,26 @@ export default function Projects({ className }: { className: string }) {
     },
     {
       title: "Banter",
-      description: { short: "A chat app.", long: "A chat app." },
+      description: {
+        short: "A real-time chat app with customizable profiles.",
+        long: "Banter is a real-time chat app that allows users to create customizable profiles, engage in instant messaging, and more. Built using React TypeScript and Tailwind, Banter provides a modern and interactive chatting experience.",
+      },
       link: "https://piratey7007.github.io/banter/",
       image: "https://piratey7007.github.io/banter/banter.png",
       video: "https://www.youtube.com/embed/",
     },
+    {
+      title: "Bazaar",
+      description: {
+        short: "A feature-rich online marketplace.",
+        long: "Bazaar is a comprehensive online marketplace that allows users to create accounts, post products, search for items based on filters, add items to their cart, and complete a simulated checkout process. Users can also review their previous orders. Bazaar was built using React TypeScript, Tailwind, NestJS, and makes use of numerous libraries to provide a seamless shopping experience.",
+      },
+      link: "https://We-Got-This-2023.github.io/bazaar/",
+      image: "https://We-Got-This-2023.github.io/bazaar/bazaar.png",
+      video: "https://www.youtube.com/embed/",
+    },
   ];
+
   return (
     <div className={"flex flex-col " + className}>
       <h1 className="left-0 right-0 mx-auto">Projects</h1>
