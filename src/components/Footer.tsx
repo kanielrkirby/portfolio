235200ea8fc6d4fd2948209d5e4aaf1d9ed97b35
Max <kanielrkirby@gmail.com>
2023-04-05 14:36:56 -0500

    Added links to social media

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,55 @@
+import { Link } from "react-router-dom";
+
 export default function Footer({ className }: { className?: string }) {
   return (
-    <div className={className}>
-      <h1>This is a footer.</h1>
+    <div className={"mt-8 " + className}>
+      <ul className="flex justify-end gap-4 p-2">
+        <li>
+          <Link
+            to="https://www.linkedin.com/in/kaniel-kirby-9b1460261/"
+            target="_blank"
+          >
+            <img
+              src="/src/assets/linkedin.png"
+              alt="LinkedIn"
+              className="h-8 cursor-pointer opacity-40 transition-all duration-150 ease-out hover:scale-110 hover:opacity-70"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link to="https://github.com/piratey7007" target="_blank">
+            <img
+              src="/src/assets/github.png"
+              alt="GitHub"
+              className="h-8 cursor-pointer opacity-40 transition-all duration-150 ease-out hover:scale-110 hover:opacity-70"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="https://discordapp.com/users/347554007750803459"
+            target="_blank"
+          >
+            <img
+              src="/src/assets/discord.png"
+              alt="Discord"
+              className="h-8 cursor-pointer opacity-40 transition-all duration-150 ease-out hover:scale-110 hover:opacity-70"
+            />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="https://www.reddit.com/user/CompetitiveAd7245"
+            target="_blank"
+          >
+            <img
+              src="/src/assets/reddit.png"
+              alt="Reddit"
+              className="h-8 cursor-pointer opacity-40 transition-all duration-150 ease-out hover:scale-110 hover:opacity-70"
+            />
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
