20af35d642d286d218b284176ea473763d46ef91
Piratey7007 <piratey7007@runbox.com>
2023-04-10 14:59:45 -0500

    Added escape button, fixed modal transitions

--- src/contexts/ModalContext.tsx
+++ src/contexts/ModalContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import useLocal from "../hooks/useLocal";
 
@@ -10,46 +10,73 @@ export const ModalContext = createContext({} as ContextI);
 
 export function ModalProvider({ children }: { children: React.ReactNode }) {
   const [modalOpen, setModalOpen] = useState(false);
+  const [hidden, setHidden] = useState(true);
   const { get, toggle } = useLocal();
 
+  function handleModalOpen(modal: boolean) {
+    if (modal) {
+      setHidden(false);
+      setTimeout(() => {
+        setModalOpen(true);
+      }, 0);
+    } else {
+      setModalOpen(false);
+      setTimeout(() => {
+        setHidden(true);
+      }, 300);
+    }
+  }
+
+  useEffect(() => {
+    onkeydown = ({ key }) => {
+      if (key === "Escape") {
+        handleModalOpen(false);
+      }
+    };
+  }, []);
+
   return (
-    <ModalContext.Provider value={{ setModalOpen }}>
+    <ModalContext.Provider value={{ setModalOpen: handleModalOpen }}>
       {children}
       {ReactDOM.createPortal(
-        <div
-          className={`fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 transition-all duration-300 ease-in-out selection:bg-red-300 ${
-            modalOpen ? "opacity-100" : "pointer-events-none opacity-0"
-          }`}
-        >
-          <div
-            className={`relative flex h-[30rem] w-[40rem] flex-col items-center gap-4 rounded-md bg-white p-16 transition-all duration-300 ease-in-out ${
-              modalOpen ? "" : "translate-y-full"
-            }`}
-          >
-            <button
-              className="group absolute top-0 right-0 z-10 h-fit w-fit cursor-pointer p-5"
-              onClick={() => setModalOpen(false)}
+        <>
+          {!hidden && (
+            <div
+              className={`fixed top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black bg-opacity-50 transition-all duration-300 ease-in-out selection:bg-red-300 ${
+                modalOpen ? "opacity-100" : "pointer-events-none opacity-0"
+              }`}
             >
-              <div className="relative block cursor-pointer text-black transition-all duration-150 after:pointer-events-none after:absolute after:top-0 after:bottom-0 after:right-0 after:left-0 after:-z-10 after:m-auto after:scale-x-0 after:scale-y-100 after:rounded-sm after:bg-black after:p-0 after:transition-all after:duration-150 after:content-[''] group-hover:text-red-500 group-hover:after:scale-x-[125%] group-hover:after:scale-y-[125%]">
-                Close
+              <div
+                className={`relative flex h-[30rem] w-[40rem] flex-col items-center gap-4 rounded-md bg-white p-16 transition-all duration-300 ease-in-out ${
+                  modalOpen ? "" : "translate-y-full"
+                }`}
+              >
+                <button
+                  className="group absolute top-0 right-0 z-10 h-fit w-fit cursor-pointer p-5"
+                  onClick={() => handleModalOpen(false)}
+                >
+                  <div className="relative block cursor-pointer text-black transition-all duration-150 after:pointer-events-none after:absolute after:top-0 after:bottom-0 after:right-0 after:left-0 after:-z-10 after:m-auto after:scale-x-0 after:scale-y-100 after:rounded-sm after:bg-black after:p-0 after:transition-all after:duration-150 after:content-[''] group-hover:text-red-500 group-hover:after:scale-x-[125%] group-hover:after:scale-y-[125%]">
+                    Close
+                  </div>
+                </button>
+                <h2 className="text-black">Settings</h2>
+                <div className="flex w-full items-center justify-around">
+                  <label className="text-black" htmlFor="background">
+                    Moving Background
+                  </label>
+                  <input
+                    className=""
+                    id="background"
+                    name="background"
+                    type="checkbox"
+                    defaultChecked={get("background")}
+                    onClick={() => toggle("background")}
+                  />
+                </div>
               </div>
-            </button>
-            <h2 className="text-black">Settings</h2>
-            <div className="flex w-full items-center justify-around">
-              <label className="text-black" htmlFor="background">
-                Moving Background
-              </label>
-              <input
-                className=""
-                id="background"
-                name="background"
-                type="checkbox"
-                defaultChecked={get("background")}
-                onClick={() => toggle("background")}
-              />
             </div>
-          </div>
-        </div>,
+          )}
+        </>,
         document.getElementById("modal-root") as HTMLElement,
       )}
     </ModalContext.Provider>
