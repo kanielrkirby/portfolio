12474484c3d00300c88b60b4b0bf9558b5f0facf
Piratey7007 <piratey7007@runbox.com>
2023-06-15 08:25:15 -0500

    CSS autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwind],
+      plugins: [tailwind, autoprefixer],
     },
   },
 });
