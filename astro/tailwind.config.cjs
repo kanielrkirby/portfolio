d89cf8adf09fe56db9377adef243df70859af339
Piratey7007 <piratey7007@runbox.com>
2023-07-12 16:24:50 -0500

    adding images

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -1,3 +1,5 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -6,6 +8,11 @@ module.exports = {
       animation: {
         "spin-slow": "spin 1.5s cubic-bezier(0.42, 0.0, 0.58, 1.0) infinite",
         spin: "spin 1s linear infinite",
+      },
+      fontFamily: {
+        body: ["Fira Mono", ...defaultTheme.fontFamily.mono],
+        display: ["Comfortaa", ...defaultTheme.fontFamily.sans],
+        card: "Courier",
       },
     },
   },
