0b4a51a6dbe0dc133941ec644be74962980bbf20
Piratey7007 <piratey7007@runbox.com>
2023-07-12 13:58:15 -0500

    Added loading screen and canvas settings

--- astro/tailwind.config.cjs
+++ astro/tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "spin-slow": "spin 1.5s cubic-bezier(0.42, 0.0, 0.58, 1.0) infinite",
+        spin: "spin 1s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};
