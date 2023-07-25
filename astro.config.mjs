b807393d23b8c380e92c65273093febed6f6f7e7
Piratey7007 <piratey7007@runbox.com>
2023-07-24 19:18:50 -0500

    Added Credits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
-import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), prefetch(), image(), tailwind(), compress()],
+  integrations: [prefetch(), image(), tailwind(), compress()],
 });
