e9eadd137dbe0438204022edbe0d9c491c851b0c
Piratey7007 <piratey7007@runbox.com>
2023-08-11 13:09:58 -0500

    added preload plugin to swup

--- src/scripts/swup.ts
+++ src/scripts/swup.ts
@@ -5,18 +5,22 @@ import SwupSlideTheme from "@swup/slide-theme";
 import SwupScriptsPlugin from "@swup/scripts-plugin";
 import SwupProgressPlugin from "@swup/progress-plugin";
 import SwupA11yPlugin from "@swup/a11y-plugin";
+import SwupPreloadPlugin from "@swup/preload-plugin";
+
+const selector = "main:not(#business-card)";
 
 const swup = new Swup({
   plugins: [
     new SwupSlideTheme({
-      mainElement: "main",
+      mainElement: selector,
     }),
-    new SwupA11yPlugin(),
+    new SwupA11yPlugin({ contentSelector: selector }),
     new SwupHeadPlugin(),
     new SwupScriptsPlugin(),
     new SwupProgressPlugin(),
+    new SwupPreloadPlugin(),
   ],
-  containers: ["main:not(#business-card)"],
+  containers: [selector],
 });
 
 swup.hooks.before("page:load", () => closeModal("nav"));
