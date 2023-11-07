037c2d60fdf0c15e36e863db2fd6f3084d53bad8
Piratey7007 <piratey7007@runbox.com>
2023-11-06 18:22:30 -0600

    Added resume as a PDF instead of a link

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
-  resume: "https://docs.google.com/document/d/e/2PACX-1vSdbxUkU2KQlcZMpRnGIJUkxmAHUhQmmHlbtEg84pT5dwvRB2vEIaKHan4rnp849iZgaEbhv8CutHy7/pub",
+  resume: "@/assets/resume.pdf",
   linkedin: "kanielrkirby",
   phone: "325.443.6046",
   email: "kanielrkirby@runbox.com",
